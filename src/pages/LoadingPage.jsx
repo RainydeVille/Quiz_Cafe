48fd59e88ceeca979eb50f72ae3babe484e2ea88
RainydeVille <vaitkevicienegle@gmail.com>
2026-05-21 18:37:42 +0300

    Added changing messages below the loading bar

--- src/pages/LoadingPage.jsx
+++ src/pages/LoadingPage.jsx
@@ -1,9 +1,21 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "./LoadingPage.scss";
 
 export default function LoadingPage() {
-  const navigate = useNavigate();
+  const loadingMessages = ["Brewing fresh quizzes...", "Polishing coffee mugs...", "Finding your favorite seat..."];
+
+  const [message, setMessage] = useState(loadingMessages[0]);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const randomMessage = loadingMessages[Math.floor(Math.random() * loadingMessages.length)];
+
+      setMessage(randomMessage);
+    }, 2000);
+
+    return () => clearInterval(interval);
+  }, []);
 
   return (
     <div className="loadingPage">
@@ -13,7 +25,7 @@ export default function LoadingPage() {
       <div className="loadingWrapper">
         <img className="loadingBar" src="./progression-bar.gif" alt="loading..." />
 
-        <p className="loadingText">Preparing your table...</p>
+        <p className="loadingText">{message}</p>
       </div>
     </div>
   );
