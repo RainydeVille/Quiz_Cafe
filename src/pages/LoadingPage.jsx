abe0193dfb6d666e223a96346ec8f23bc2361815
RainydeVille <vaitkevicienegle@gmail.com>
2026-05-21 18:06:03 +0300

    minimal scss

--- src/pages/LoadingPage.jsx
+++ src/pages/LoadingPage.jsx
@@ -6,14 +6,14 @@ export default function LoadingPage() {
   const navigate = useNavigate();
 
   return (
-    <div className="loading-page">
+    <div className="loadingPage">
       <div className="logoWrapper">
         <h1 className="logo">Welcome to Quiz Café</h1>
       </div>
       <div className="loadingWrapper">
         <img className="loadingBar" src="./progression-bar.gif" alt="loading..." />
 
-        <p className="loading-text">Preparing your table...</p>
+        <p className="loadingText">Preparing your table...</p>
       </div>
     </div>
   );
