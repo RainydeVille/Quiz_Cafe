674b5071e6bc109bcec675176cc4b275a659eff1
RainydeVille <vaitkevicienegle@gmail.com>
2026-06-03 16:09:37 +0300

    Made and integrated buttons with useState

--- src/pages/LoadingPage.jsx
+++ src/pages/LoadingPage.jsx
@@ -3,10 +3,20 @@ import { useNavigate } from "react-router-dom";
 import "./LoadingPage.scss";
 
 export default function LoadingPage() {
+  //for messages
   const loadingMessages = ["Brewing fresh quizzes...", "Polishing coffee mugs...", "Finding your favorite seat..."];
-
   const [message, setMessage] = useState(loadingMessages[0]);
+  //for buttons
+  const buttonImages = {
+    locked: "./locked.png",
+    idle: "./idle.png",
+    hover: "./hovered.png",
+    pressed: "./pressed.png",
+  };
+  const [isLoading, setIsLoading] = useState(true);
+  const [buttonState, setButtonState] = useState("locked");
 
+  //messages
   useEffect(() => {
     const interval = setInterval(() => {
       const randomMessage = loadingMessages[Math.floor(Math.random() * loadingMessages.length)];
@@ -16,6 +26,45 @@ export default function LoadingPage() {
 
     return () => clearInterval(interval);
   }, []);
+
+  //BUTTON HANDLING
+  //from locked to idle
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+      setButtonState("idle");
+    }, 3000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  //when hovered over
+  const handleMouseEnter = () => {
+    if (!isLoading) {
+      setButtonState("hover");
+    }
+  };
+
+  //unhover
+  const handleMouseLeave = () => {
+    if (!isLoading) {
+      setButtonState("idle");
+    }
+  };
+
+  //on click
+  const handleMouseDown = () => {
+    if (!isLoading) {
+      setButtonState("pressed");
+    }
+  };
+
+  //unclick
+  const handleMouseUp = () => {
+    if (!isLoading) {
+      setButtonState("hover");
+    }
+  };
 
   return (
     <div className="loadingPage">
@@ -28,7 +77,15 @@ export default function LoadingPage() {
         <p className="loadingText">{message}</p>
       </div>
       <div className="startWrapper">
-        <img className="startBtn" src="./button1.png" alt="start button" />
+        <img
+          className="startBtn"
+          src={buttonImages[buttonState]}
+          alt="Go to Quiz Cafe"
+          onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave}
+          onMouseDown={handleMouseDown}
+          onMouseUp={handleMouseUp}
+        />
       </div>
     </div>
   );
